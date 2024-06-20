43cf2056bb98ba3e1ec8cf1e54d74b54e5ece3cd
Zei33 <zei33@users.noreply.github.com>
2024-06-21 06:57:06 +1000

    Added cheat types.

---
+++ types/index.d.ts
@@ -0,0 +1,2 @@
+declare function _exports(source: any, options: any): Promise<any>;
+export = _exports;
---
+++ types/util.d.ts
@@ -0,0 +1,2 @@
+export function streamToBuffer(stream: any): any;
+export function removeUndefined(dimensions: any): any;
